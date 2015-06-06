79fd23d4d792296dc17129926cee5efecec529ab
Yohei <yoohei2323@gmail.com>
2015-06-06 15:39:26 +0900

    Initial Commit

---
+++ WarkAround/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WarkAround
+//
+//  Created by Yohei Fusayasu on 6/6/15.
+//  Copyright (c) 2015 Tachikoma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
